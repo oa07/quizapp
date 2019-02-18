1b4dc4b3604dc86b373c32e9c450e08f60c23876
sifatul <sifii2013@gmail.com>
2019-02-18 23:37:20 +0900

    home page buttons style updated

--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -28,29 +28,32 @@ export default class HomeScreen extends Component{
       return(
         <View style = {styles.container}>
           <View style = {styles.middlecontainer}>
-          <Button
-            title="Grammer"
-            onPress={() => navigate.navigate('Grammer')}
-          />
-          <TouchableHighlight onPress={() => this.setState({ visibleModal: 3})}>
-          <Image style={styles.dictionaryIcon} source={require('../assets/Images/ChatHead.png')} />
-          </TouchableHighlight>
-          <Modal
-            isVisible={this.state.visibleModal === 3}
-            animationInTiming={2000}
-            animationOutTiming={2000}
-            backdropTransitionInTiming={2000}
-            backdropTransitionOutTiming={2000}
-          >
-            {this._renderModalContent()}
-          </Modal>
+  
+            <TouchableHighlight onPress={() => this.setState({ visibleModal: 3})}>
+              <Image style={styles.dictionaryIcon} source={require('../assets/Images/ChatHead.png')} />
+            </TouchableHighlight>
+            <Modal
+              isVisible={this.state.visibleModal === 3}
+              animationInTiming={2000}
+              animationOutTiming={2000}
+              backdropTransitionInTiming={2000}
+              backdropTransitionOutTiming={2000}
+            >
+              {this._renderModalContent()}
+            </Modal>
           </View>
-          <View style = {styles.bottomcontainer}>
-          <Button
-          title="Vocabulary"
-          onPress={() => navigate.navigate('Vocabulary')}
-          />
-          </View>
+
+          
+            <View style = {styles.home_main_buttons_container}>
+  
+              <TouchableHighlight style={styles.fullWidthButton} onPress={() => navigate.navigate('Grammer')}>
+                  <Text style={styles.fullWidthButtonText}>Grammer</Text>
+              </TouchableHighlight>
+              <TouchableHighlight style={styles.fullWidthButton} onPress={() => navigate.navigate('Vocabulary')}>
+                  <Text style={styles.fullWidthButtonText}>Vocabulary</Text>
+              </TouchableHighlight>
+            
+            </View>
           </View>
       );
     }
@@ -58,24 +61,40 @@ export default class HomeScreen extends Component{
 
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#Fc5F',
+      flex: 1, 
+      backgroundColor: 'white',
       padding: 20
     },
-    middlecontainer: {
-      flex: 1.5,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    bottomcontainer:{
-      flex: 2,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    quizButton: {
+    fullWidthButton: {
+      borderRadius: 5,
+      width: "100%", 
+      backgroundColor: "#4CAF50",
+      paddingTop: 20,
+      paddingBottom: 20,
+      paddingRight:10,
+      paddingLeft:10,    
+      marginTop : 10,
+      alignItems: 'center'
       
+    },
+    fullWidthButtonText: {
+      fontSize:20,
+      color: 'white'
+    },
+    middlecontainer: {
+      position: 'absolute', 
+      top: 0, 
+      right: 0,
+      margin:20,
+     
+    },
+    home_main_buttons_container:{
+      flex: 2,
+      width: "100%", 
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    quizButton: {      
       justifyContent: 'space-between',
       alignContent: 'flex-start',
       flexDirection: 'column',
@@ -90,8 +109,8 @@ export default class HomeScreen extends Component{
       borderColor: 'rgba(0, 0, 0, 0.1)',
     },
     dictionaryIcon: {
-      height:50,
-      width: 50,
+      height:70,
+      width: 70,
 
     }
   });
