46693d63429ed4af017a1e8d4338369f5f23f952
sifatul <sifii2013@gmail.com>
2019-02-27 21:01:51 +0900

    chathead is taken out as component

--- app/components/DictionaryComponent.js
+++ app/components/DictionaryComponent.js
@@ -10,6 +10,7 @@ import {
 import Icon from "react-native-vector-icons/Ionicons";
 import * as Animatable from 'react-native-animatable'
 import HomePageStyle from '../styles/HomePageStyle' ;
+import Modal from 'react-native-modal';
 
 
 const eng2bangla = require("./eng2bangla.json");
@@ -23,44 +24,99 @@ export default class DictionaryComponent extends Component {
       input: "",
       output: "",
       from: "en",
-      to: "bn"
+      to: "bn",
+      visible: false,
     };
+
   }
+  toggleModal = () => {
+    console.log("i am here");
+    this.setState({visible: !this.state.visible});
+  }
+
+
 
   render() {
     return (
-      <View style={{ width: '100%'}}>
+      <Modal
+        style={{ width: "100%", left: 0, padding: 0, margin: 0 }}
+        isVisible={this.state.visible}
+        animationInTiming={2000}
+        animationOutTiming={2000}
+        backdropTransitionInTiming={2000}
+        backdropTransitionOutTiming={2000}
+      >
+        <View style={HomePageStyle.modalContent}>
+          <View style={{ width: "100%" }}>
+            <View style={HomePageStyle.modalHeaderContainer}>
+              <Animatable.View
+                animation={
+                  this.state.searchBarFocused ? "fadeInLeft" : "fadeInRight"
+                }
+                duration={400}
+                style={HomePageStyle.modalBackBtnContainer}
+              >
+                <Icon
+                  name={"ios-arrow-round-back"}
+                  onPress={this.toggleModal}
+                  style={HomePageStyle.modalBackBtn}
+                />
+              </Animatable.View>
 
-      <View style={HomePageStyle.modalHeaderContainer }>
-                              <Animatable.View animation={this.state.searchBarFocused ? "fadeInLeft" : "fadeInRight"} duration={400} style={HomePageStyle.modalBackBtnContainer}>
-                                 <Icon name={  "ios-arrow-round-back"}
-                                 style={HomePageStyle.modalBackBtn} />
-                               </Animatable.View>
+              <Animatable.View
+                animation="slideInRight"
+                duration={500}
+                style={HomePageStyle.chatheadSearchBlock}
+              >
+                <Animatable.View
+                  animation={
+                    this.state.searchBarFocused
+                      ? "fadeInLeft"
+                      : "fadeInRight"
+                  }
+                  duration={400}
+                >
+                  <Icon
+                    name={
+                      this.state.searchBarFocused
+                        ? "md-arrow-back"
+                        : "ios-search"
+                    }
+                    style={{
+                      fontSize: 20,
+                      margin: 5,
+                      fontWeight: "bold",
+                      color: "black"
+                    }}
+                  />
+                </Animatable.View>
+                <TextInput
+                  text={this.state.input}
+                  autoFocus={true}
+                  onChangeText={e => this.setState({ input: e })}
+                  onSubmitEditing={e => this.showMeaning(e)}
+                  style={{
+                    fontSize: 20,
+                    padding: 2,
+                    flex: 1,
+                    color: "black"
+                  }}
+                />
+              </Animatable.View>
+            </View>
 
-                             <Animatable.View animation="slideInRight" duration={500} style={HomePageStyle.chatheadSearchBlock}>
-                                 <Animatable.View animation={this.state.searchBarFocused ? "fadeInLeft" : "fadeInRight"} duration={400}>
-                                   <Icon name={this.state.searchBarFocused ? "md-arrow-back" : "ios-search"} style={{ fontSize: 20, margin:5, fontWeight:'bold',color:'black' }} />
-                                 </Animatable.View>
-                                     <TextInput
-                                           text={this.state.input}
-                                           autoFocus={true}
-                                           onChangeText={e => this.setState({ input: e })}
-                                           onSubmitEditing={e => this.showMeaning(e)}
-                                           style={{ fontSize: 20, padding:2,  flex: 1,color:'black' }}
-                                         />
-                            </Animatable.View>
-      </View>
-
-
-        {
-            this.state.output != ''?
-                <View style={HomePageStyle.chatheadAnswerBlock}>
-                   <Text style={{ fontSize:20, padding:10,  fontWeight: 'bold' }}>{this.state.output}</Text>
-                </View>: null
-        }
-
-
-      </View>
+            {this.state.output != "" ? (
+              <View style={HomePageStyle.chatheadAnswerBlock}>
+                <Text
+                  style={{ fontSize: 20, padding: 10, fontWeight: "bold" }}
+                >
+                  {this.state.output}
+                </Text>
+              </View>
+            ) : null}
+          </View>
+        </View>
+      </Modal>
     );
   }
   showMeaning(e) {
--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { Text, View, Button, TouchableOpacity, ImageBackground,  TouchableHighlight, Image } from 'react-native';
-import Modal from 'react-native-modal';
 import DictionaryComponent from '../components/DictionaryComponent';
 import styles from '../styles/HomePageStyle' ;
 import commonStyles from '../styles/CommonStyle' ;
@@ -8,9 +7,7 @@ import Icon from "react-native-vector-icons/Ionicons";
 
 export default class HomeScreen extends Component{
 
-    state = {
-      visibleModal: null,
-    };
+    
     _renderButton = (text, onPress) => (
       <TouchableOpacity onPress={onPress}>
         <View style={styles.button}>
@@ -21,53 +18,47 @@ export default class HomeScreen extends Component{
 
     render(){
       const navigate  = this.props.navigation;
-      return(
-            <ImageBackground source={require('../assets/Images/Background.jpg')} style={commonStyles.backgroundImage}>
+      return (
+        <ImageBackground
+          source={require("../assets/Images/Background.jpg")}
+          style={commonStyles.backgroundImage}
+        >
+          <DictionaryComponent
+            ref={ref => {
+              this.myModal = ref;
+            }}
+          />
 
-            <Modal
-                style={{ width: '100%'}}
-                isVisible={this.state.visibleModal === 3}
-                animationInTiming={2000}
-                animationOutTiming={2000}
-                backdropTransitionInTiming={2000}
-                backdropTransitionOutTiming={2000}
-                style={{left:0, padding:0,margin:0}}>
-                <View style={styles.modalContent} >
-
-                   <DictionaryComponent/>
-
-                </View>
-            </Modal>
-             <View style = {styles.middlecontainer}>
-                <TouchableHighlight
-                underlayColor='transparent'
-                onPress={() => this.setState({ visibleModal: 3})}>
-                  <Image style={styles.dictionaryIcon} source={require('../assets/Images/ChatHead.png')} />
-                </TouchableHighlight>
-
-             </View>
-            <View style = {commonStyles.container}>
-                    <View style = {styles.home_main_buttons_container}>
-                      <TouchableHighlight
-                        underlayColor='#689F38'
-                        style={ styles.fullWidthButton }
-                        onPress ={() => navigate.navigate('Grammer')}
-                       >
-                          <Text style={styles.fullWidthButtonText}>Grammer</Text>
-                      </TouchableHighlight>
-                      <TouchableHighlight
-                          underlayColor='#689F38'
-                          style={styles.fullWidthButton}
-                          onPress={() => navigate.navigate('Vocabulary')}
-                      >
-                          <Text style={styles.fullWidthButtonText}>Vocabulary</Text>
-                      </TouchableHighlight>
-
-                    </View>
+          <View style={styles.middlecontainer}>
+            <TouchableHighlight
+              underlayColor="transparent"
+              onPress={() =>  this.myModal.toggleModal()}
+            >
+              <Image
+                style={styles.dictionaryIcon}
+                source={require("../assets/Images/ChatHead.png")}
+              />
+            </TouchableHighlight>
+          </View>
+          <View style={commonStyles.container}>
+            <View style={styles.home_main_buttons_container}>
+              <TouchableHighlight
+                underlayColor="#689F38"
+                style={styles.fullWidthButton}
+                onPress={() => navigate.navigate("Grammer")}
+              >
+                <Text style={styles.fullWidthButtonText}>Grammer</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                underlayColor="#689F38"
+                style={styles.fullWidthButton}
+                onPress={() => navigate.navigate("Vocabulary")}
+              >
+                <Text style={styles.fullWidthButtonText}>Vocabulary</Text>
+              </TouchableHighlight>
             </View>
-            </ImageBackground>
-
-
+          </View>
+        </ImageBackground>
       );
     }
   }
